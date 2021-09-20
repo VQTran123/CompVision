7aedc8c0c52c3634882b941adec6d9bc73403131
sggu <shgudwls99@gmail.com>
2021-09-19 22:44:40 -0400

    hw1 testing

--- HW1/main.py
+++ HW1/main.py
@@ -3,10 +3,14 @@ import scipy.sparse
 import cv2
 
 if __name__ == "__main__":
-    source = input("Enter file name of source image")
-    target = input("Enter file name of target image")
-    mask = input("Enter file name of mask")
-
-    source_image = cv2.imread(source)
+    source = input("dog.png")
+    target = input("beach.png")
+    mask = input("mask1.png")
+    
+    source_image = cv2.imread("dog.png")
     target_image = cv2.imread(target)
-    mask_image = cv2.imread(mask, cv2.IMREAD_GRAYSCALE)
+    mask_image = cv2.imread("mask1.png", cv2.IMREAD_GRAYSCALE)
+
+    print(len(source_image[0]))
+    #print(target_image[0])
+    source_image = cv2.imwrite("dog.png",mask_image[0])
