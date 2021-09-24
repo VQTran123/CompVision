64bfa4b46a3d635c611bde0d8b6279d544589352
Vinh <vinhtran2001@gmail.com>
2021-09-24 14:25:26 -0400

    Adjusted constants

--- HW2/Q1/main.py
+++ HW2/Q1/main.py
@@ -3,16 +3,18 @@ import scipy.sparse
 import cv2
 
 def matting(image, a1, a2):
-    green_channel = np.atleast_2d(image[:,:,0])
-    red_channel = np.atleast_2d(image[:,:,2])
-    mat = np.copy(green_channel).astype(float)
-    for i, k in enumerate(green_channel):
-        mat[k] = 1 - a1*(green_channel[k] - a2*red_channel[k])
+    green_channel = np.atleast_2d(image[:,:,1]).astype(float)/255
+    red_channel = np.atleast_2d(image[:,:,0]).astype(float)/255
+    print(green_channel)
+    mat = 1.0 - a1*(green_channel - green_channel*a2)
+    print(mat)
+    mat = mat*255
     return mat
 
 if __name__ == "__main__":
-    source_name = input("Enter file name of source image ")
+    #source_name = input("Enter file name of source image ")
+    source_name = "testing_1.png"
     source_image = cv2.imread(source_name)
     source = np.atleast_3d(source_image).astype(float)
-    mat = matting(source, 0.5, 1)
-    cv2.imwrite("output.jpg", mat)
+    mat = matting(source, 1.0, 0.01)
+    cv2.imwrite("output.png", mat)
