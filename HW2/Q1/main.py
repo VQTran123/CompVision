b98069c80c77bcb32f1cf38cafe92d009bf23ee5
Vinh <vinhtran2001@gmail.com>
2021-10-04 14:25:51 -0400

    Added user options

--- HW2/Q1/main.py
+++ HW2/Q1/main.py
@@ -19,10 +19,11 @@ def matting(image, a1, a2):
     return mat
 
 if __name__ == "__main__":
-    #source_name = input("Enter file name of source image ")
+    source_name = input("Enter file name of source image ")
+    target_name = input("Enter file name of target image ")
     #source_name = "testing_1.png"
-    target_name = "howl.jpg"
-    source_name = "radke.mp4"
+    #target_name = "howl.jpg"
+    #source_name = "radke.mp4"
     #source_name = "green.jpg"
     if source_name[len(source_name)-4:] == ".png" or source_name[len(source_name)-4:] == ".jpg":
         source_image = cv2.imread(source_name)
@@ -37,39 +38,39 @@ if __name__ == "__main__":
         result = cv2.merge(results)
         cv2.imwrite("output.jpg", result)
     else:
-        #source_video = cv2.VideoCapture(source_name)
-        img_array = []
-        for i in range(1,173):
-            img = cv2.imread("output_" + str(i) + ".jpg")
-            height, width, layers = img.shape
-            img_array.append(img)
-        out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (width, height))
-        target_image = cv2.imread(target_name)
-        target = np.atleast_3d(target_image).astype(float)
-        for i in range(len(img_array)):
-            out.write(img_array[i])
-        out.release
-        '''
-        while(source_video.isOpened()):
-            i += 1
-            ret, frame = source_video.read()
-            if ret == True:
-                source = np.atleast_3d(frame).astype(float)
-                mat = matting(source, 1, 1)
-                results = []
-                for channel in range(3):
-                    results.append(composite.poisson_blend(source[:,:,channel], target[:,:,channel], mat))
-                result = cv2.merge(results)
-                cv2.imwrite("output_" + str(i) + ".jpg", result)
-                #out.write(result)
-                #cv2.imshow("frame",result)
-                #if cv2.waitKey(5) and 0xFF == ord('q'):
-                #    break
-            else:
-                break
-        source_video.release()
-        '''
-        #out.release()
+        option = input("Press 1 to store images or any other key to composite and create video")
+        if option == 1:
+            source_video = cv2.VideoCapture(source_name)
+            target_image = cv2.imread(target_name)
+            target = np.atleast_3d(target_image).astype(float)
+            i = 0
+            while(source_video.isOpened()):
+                i += 1
+                ret, frame = source_video.read()
+                if ret == True:
+                    source = np.atleast_3d(frame).astype(float)
+                    mat = matting(source, 1, 1)
+                    results = []
+                    for channel in range(3):
+                        results.append(composite.poisson_blend(source[:,:,channel], target[:,:,channel], mat))
+                    result = cv2.merge(results)
+                    cv2.imwrite("output_" + str(i) + ".jpg", result)
+                else:
+                    break
+            source_video.release()
+        else:
+            img_array = []
+            for i in range(1,173):
+                img = cv2.imread("output_" + str(i) + ".jpg")
+                height, width, layers = img.shape
+                img_array.append(img)
+            out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (width, height))
+            target_image = cv2.imread(target_name)
+            target = np.atleast_3d(target_image).astype(float)
+            for i in range(len(img_array)):
+                out.write(img_array[i])
+            out.release
+
         
 
     
